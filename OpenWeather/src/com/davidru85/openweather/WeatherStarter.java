5af66856c57b7779a367afd7b2d00171396cfdb1
David Ruiz Urraca <davidru85@gmail.com>
2014-01-24 18:22:38 +0100

    When the service starts, there is no previous notifications

--- OpenWeather/src/com/davidru85/openweather/WeatherStarter.java
+++ OpenWeather/src/com/davidru85/openweather/WeatherStarter.java
@@ -3,6 +3,8 @@ package com.davidru85.openweather;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 
 public class WeatherStarter extends BroadcastReceiver {
 
@@ -11,6 +13,12 @@ public class WeatherStarter extends BroadcastReceiver {
 		// TODO Auto-generated method stub
 		Intent i = new Intent(context, ServiceWeather.class);  // aqui llama a la clase que comienza la app
 		i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		SharedPreferences prefs = context.getSharedPreferences(Values.getPrefs(), Context.MODE_PRIVATE);
+		
+		Editor edit = prefs.edit();
+		edit.putBoolean(Values.getPrevNotif(), false);
+		edit.commit();
+		
 		context.startService(i);
 	}
 
