2aa0911294c2d2141dee1648d9899f95e48b20b8
David Ruiz Urraca <davidru85@gmail.com>
2014-01-24 18:14:13 +0100

    Recived full forecast instead of just actual weather

--- OpenWeather/src/com/davidru85/openweather/WeatherAsyncTask.java
+++ OpenWeather/src/com/davidru85/openweather/WeatherAsyncTask.java
@@ -4,16 +4,18 @@ import org.json.JSONObject;
 
 import android.os.AsyncTask;
 
-public class WeatherAsyncTask extends AsyncTask<JsonParser, Integer, Weather> {
+public class WeatherAsyncTask extends AsyncTask<JsonParser, Integer, Weather[]> {
 
 	//private static final String LogDavid = "OpenWeather";
 
 	@Override
-	protected Weather doInBackground(JsonParser... parser) {
-		Weather weather = new Weather();
+	protected Weather[] doInBackground(JsonParser... parser) {
+		//Weather weather = new Weather();
+		Weather[] weathers;
 		JSONObject jsonObject = parser[0].getJSONFromUrl();
-		weather = Conversor.jsonToWeather(jsonObject);
-        return weather;
+		//weather = Conversor.jsonToWeather(jsonObject);
+		weathers = Conversor.jsonToForecast(jsonObject);
+        return weathers;
 	}
 
 }
