19a053ad0d95d56fe17c18e705ab77d9d87b2514
David Ruiz Urraca <davidru85@gmail.com>
2014-01-24 18:13:11 +0100

    New static values for preferences name and previous notification preference

--- OpenWeather/src/com/davidru85/openweather/Values.java
+++ OpenWeather/src/com/davidru85/openweather/Values.java
@@ -7,7 +7,7 @@ public class Values {
 	public static final long TEN_SECONDS = 1000 * 10;
 	public static final long TEN_MINUTES = 1000 * 60 * 10;
 
-	private static final long elapsed_time = ONE_HOUR;
+	public static final long elapsed_time = ONE_HOUR;
 	
 	private static final String weatherURL = "http://api.openweathermap.org/data/2.5/weather?";
 	private static final String weatherURL2 = "http://api.openweathermap.org/data/2.5/find?cnt=1&";
@@ -18,6 +18,10 @@ public class Values {
 	private static final String profileFacebook = "http://facebook.com/davidru85";
 	private static final String profileGoogle = "https://plus.google.com/115987355481602001725";
 	private static final String profileLinkedin = "http://linkedin.com/in/davidru85/";
+	
+	private static final String PREFS = "preferences_activity";
+	
+	private static final String prev_notif = "PreviousNotification";
 	
 	public static long getElapsedTime (){
 		return elapsed_time;
@@ -49,5 +53,13 @@ public class Values {
 
 	public static String getProfilelinkedin() {
 		return profileLinkedin;
+	}
+
+	public static String getPrefs() {
+		return PREFS;
+	}
+
+	public static String getPrevNotif() {
+		return prev_notif;
 	}
 }
