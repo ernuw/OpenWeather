25479cec896918fc8dd7c96dc55b8d3eca4b380b
David Ruiz Urraca <davidru85@gmail.com>
2014-03-04 17:57:51 +0100

    Clean code

--- OpenWeather/src/com/davidru85/openweather/WeatherPreferences.java
+++ OpenWeather/src/com/davidru85/openweather/WeatherPreferences.java
@@ -1,17 +1,13 @@
 package com.davidru85.openweather;
 
 import android.os.Bundle;
-import android.preference.EditTextPreference;
-import android.preference.Preference;
 import android.preference.PreferenceActivity;
 
 public class WeatherPreferences extends PreferenceActivity {
 	@SuppressWarnings("deprecation")
+	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.preferences_activity);
-		/*EditTextPreference edit = (EditTextPreference) findViewById(R.xml.preferences_activity.
-		Preference last_update_update = findPreference("last_update_update");
-		last_update_update.setSummary("asd");*/
 	}
 }
